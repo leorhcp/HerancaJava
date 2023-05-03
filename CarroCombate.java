a1d198ecb4c84e1ee3ceb6f5e7e12639f40cd65a
Leonel <leorhcp1@gmail.com>
2023-05-02 23:05:48 -0300

    Herança OK

--- CarroCombate.java
+++ CarroCombate.java
@@ -1,15 +1,36 @@
 public class CarroCombate extends Carro{
-    private int municao;
+    private int municao = 100;
+    private int atirar;
 
 
     public CarroCombate(String marca, String modelo){
         super(marca, modelo);
+        setBlindado(true);
+        setArmadura(100);
+        setMotor(3.0f);
+        setArma(true);
     }
     public int getMunicao(){
         return this.municao;
     }
     public void setMunicao(int municao){
         this.municao=municao;
+    }
+    public int getAtirar(){
+        return this.atirar;
+    }
+    public void setAtirar(int atirar){
+        this.municao--;
+        this.atirar=atirar;
+    }
+
+    public void info(){
+        super.info();
+        System.out.println("Armadura: "+ getArmadura());
+        System.out.println("Motor: " + getMotor());
+        System.out.println("Arma: "+ getArma());
+
+        
     }
     
 
--- CursoJava.java
+++ CursoJava.java
@@ -3,9 +3,11 @@ public class CursoJava{
         
         Carro c1 = new Carro("Fiat","Uno");
         Carro c2 = new Carro("VolksWagen","Gol");
+        CarroCombate c3 = new CarroCombate("Dodge", "RAM");
 
         c1.info();
         c2.info();
+        c3.info();
 
 
     }
